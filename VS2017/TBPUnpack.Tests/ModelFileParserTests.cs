c5b4103f9320738d1b9af979d4ba674b351e3f5b
dawiddyrcz <dawid.dyrcz92@gmail.com>
2019-09-28 20:40:52 +0200

    Created new unit tests

--- VS2017/TBPUnpack.Tests/ModelFileParserTests.cs
+++ VS2017/TBPUnpack.Tests/ModelFileParserTests.cs
@@ -1,10 +1,6 @@
 ﻿using NUnit.Framework;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TBPUnpack.Tests
 {
@@ -16,23 +12,77 @@ namespace TBPUnpack.Tests
         }
 
         [Test]
-        public void ParseModelFile1_ShouldParse()
+        public void ParseModelFile1_Deserialize_ShouldParse()
         {
             var filePath = Path.Combine(GetTestDir(), "ModelFileXML1.txt");
             var modelFile = ModelFile.Deserialize(filePath);
             Assert.AreEqual("StpFile.stp", modelFile.Name);
             Assert.AreEqual("files/Models/845f3197-4722-4248-b160-c6451f18db52.stp", modelFile.Location);
-
         }
 
         [Test]
-        public void ParseNotModelFile_ShouldNotParse()
+        public void ParseModelFile1_TryParse_ShouldParse()
+        {
+            var filePath = Path.Combine(GetTestDir(), "ModelFileXML1.txt");
+            var result = ModelFile.TryParse(filePath, out ModelFile modelFile);
+            Assert.IsTrue(result);
+            Assert.AreEqual("StpFile.stp", modelFile.Name);
+            Assert.AreEqual("files/Models/845f3197-4722-4248-b160-c6451f18db52.stp", modelFile.Location);
+        }
+
+        [Test]
+        public void ParseModelFile2_Deserialize_ShouldParse()
+        {
+            var filePath = Path.Combine(GetTestDir(), "ModelFileXML2.txt");
+            var modelFile = ModelFile.Deserialize(filePath);
+            Assert.AreEqual("IFCZIPModel.ifcZIP", modelFile.Name);
+            Assert.AreEqual("files/Models/0eafb97a-621b-4565-9e2f-c78ab2c0c5f9.ifcZIP", modelFile.Location);
+        }
+
+        [Test]
+        public void ParseModelFile2_TryParse_ShouldParse()
+        {
+            var filePath = Path.Combine(GetTestDir(), "ModelFileXML2.txt");
+            var result = ModelFile.TryParse(filePath, out ModelFile modelFile);
+            Assert.IsTrue(result);
+            Assert.AreEqual("IFCZIPModel.ifcZIP", modelFile.Name);
+            Assert.AreEqual("files/Models/0eafb97a-621b-4565-9e2f-c78ab2c0c5f9.ifcZIP", modelFile.Location);
+        }
+
+        [Test]
+        public void ParseNotModelFile1_Deserialize_ShouldThrowException()
         {
             Assert.Throws<InvalidOperationException>(() =>
             {
                 var filePath = Path.Combine(GetTestDir(), "ModelGroupXML1.txt");
                 var modelFile = ModelFile.Deserialize(filePath);
             });
+        }
+
+        [Test]
+        public void ParseNotModelFile1_TryParse_ShouldThrowException()
+        {
+            var filePath = Path.Combine(GetTestDir(), "ModelGroupXML1.txt");
+            var result = ModelFile.TryParse(filePath, out ModelFile modelFile);
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void ParseNotModelFile2_Deserialize_ShouldThrowException()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                var filePath = Path.Combine(GetTestDir(), "LinkXML1.txt");
+                var modelFile = ModelFile.Deserialize(filePath);
+            });
+        }
+
+        [Test]
+        public void ParseNotModelFile2_TryParse_ShouldThrowException()
+        {
+            var filePath = Path.Combine(GetTestDir(), "LinkXML1.txt");
+            var result = ModelFile.TryParse(filePath, out ModelFile modelFile);
+            Assert.IsFalse(result);
         }
     }
 }
--- VS2017/TBPUnpack/ModelFile.cs
+++ VS2017/TBPUnpack/ModelFile.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml.Serialization;
 
 namespace TBPUnpack
@@ -17,6 +13,20 @@ namespace TBPUnpack
         public ModelFile()
         {
 
+        }
+
+        public static bool TryParse(string inputFile, out ModelFile modelFile)
+        {
+            try
+            {
+                modelFile = Deserialize(inputFile);
+                return true;
+            }
+            catch (Exception)
+            {
+                modelFile = null;
+                return false;
+            }
         }
 
         public static ModelFile Deserialize(string inputFile)
